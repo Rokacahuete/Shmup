7e0c6a1418944454ffe1ab6b62184f464a193086
Roka Perso <rokaofficiel@gmail.com>
2026-05-30 12:01:26 +0200

    Reworking crow

--- Scripts/Entities/Enemies/Crow.cs
+++ Scripts/Entities/Enemies/Crow.cs
@@ -8,15 +8,18 @@ public partial class Crow : Enemy {
 
 	// Variables
 	[Export] private float _rotationMultiplicator = 0f;
+	[Export] private Vector2 _target = Vector2.Zero;
 
 	// Functions
 	public override void _Ready() {
 		base._Ready();
+
+		_target *= GameManager.screenSize;
 	}
 
 	public override void _Process(double pDelta) {
 		float lDelta = (float)pDelta;
-		Rotation += GetAngleTo(Player.instance.GlobalPosition) * _rotationMultiplicator * lDelta;
+		Rotation += GetAngleTo(_target) * _rotationMultiplicator * lDelta;
 
 		base._Process(pDelta);
 	}
